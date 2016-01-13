60c47ac8efd0cf73cce80ed999bc4cf9d91555f2
Joshua McKinney <mckinneyj@byui.edu>
2016-01-13 15:54:38 -0700

    initial commit - works with old svg.js

---
+++ SVGMap.js
@@ -0,0 +1,139 @@
+var index = 15;
+      var pics = [
+         {fileName: 'badge1.png', x:  88, y: 301, dx: 50, dy: -50, scalePic:0.29, animateScale:2},
+         {fileName: 'badge2.png', x:  47, y: 163, dx: 50, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName: 'badge3.png', x: 174, y: 133, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName: 'badge4.png', x: 209, y: 303, dx:  0, dy: -50, scalePic:0.29, animateScale:2},
+         {fileName: 'badge5.png', x: 343, y: 226, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName: 'badge6.png', x: 256, y:  47, dx:  0, dy:  50, scalePic:0.29, animateScale:2},
+         {fileName: 'badge7.png', x: 357, y: 124, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName: 'badge8.png', x: 437, y:  47, dx:  0, dy:  50, scalePic:0.29, animateScale:2},
+         {fileName: 'badge9.png', x: 545, y:  69, dx:  0, dy:  30, scalePic:0.29, animateScale:2},
+         {fileName:'badge10.png', x: 450, y: 171, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName:'badge11.png', x: 483, y: 282, dx:  0, dy: -30, scalePic:0.29, animateScale:2},
+         {fileName:'badge12.png', x: 584, y: 272, dx:  0, dy: -20, scalePic:0.29, animateScale:2},
+         {fileName:'badge13.png', x: 624, y: 169, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName:'badge14.png', x: 715, y: 265, dx:  0, dy: -15, scalePic:0.29, animateScale:2},
+         {fileName:'badge15.png', x: 762, y:  77, dx:  0, dy:  20, scalePic:0.29, animateScale:2},
+         {fileName:'badge16.png', x: 884, y:  86, dx:  0, dy:  10, scalePic:0.29, animateScale:2},
+         {fileName:'badge17.png', x: 838, y: 210, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName:'badge18.png', x: 938, y: 292, dx:  0, dy: -40, scalePic:0.29, animateScale:2},
+         {fileName:'badge19.png', x: 970, y: 147, dx:  0, dy:   0, scalePic:0.29, animateScale:2},
+         {fileName:'badge20.png', x:1005, y:  47, dx:  0, dy:  50, scalePic:0.29, animateScale:2}
+      ]
+      
+      var picLocation = "../Real/BOMscorm/Content/SCO1/pictures/";
+      var pathLenth = 0;
+      
+      function mapStartUp(){
+         var draw = SVG('map').size(1104, 350)
+         
+         //setup event
+         SVG.registerEvent('jam:goBack');
+         
+         //background
+         draw.image(picLocation + 'FantasyMap.png');
+         //draw.image('BadgesBOM/Map-Prototype.png').transform('matrix', '0.96,0,0,0.96,0,0');
+         //line
+         draw.path("M49,349.9C34.7,335.3,11.7,206.3,118.7,194c0,0,43.5-9.5,78.7,30c3.4,3.8,12.5,19.1,24,29.3c15,13.4,31,8.3,31,8.3l21.7-1.3c0,0,28.7,3.6,15.1-28.7c-2.5-5.8-8.8-19-8.8-19s-3.6-7.9-10.3-13.7c-6.5-5.6-15.7-22.2,0.5-24.8c17.3-2.8,41-9.3,20.8-48.9C270.7,84.8,313.3,67,325,65c15.8-2.6,31-1.8,88.7,30c6.9,3.8,17.5,12.9,60,20.3c46.7,8.2,55,27.3,58.1,40.3c2.3,9.7,5.2,22.2-19.8,39c-25.1,17-29.2,42.7,20.4,31.3c10.3-2.3,26-9,32.1-9.7c18.3-2.2,21.4-3.5,46.5,7c31.7,13.3,39.7,15.7,39.7,15.7s26.7,9.8,27.3-18c0.4-17.8-2.5-35.7,27.3-43c33.8-8.3,49.4-21.4,59.3-30c28.4-24.7,31.5-27.1,46.4-28.8c3.9-0.4,10.5,0.1,16.5,0.5c13.9,1,26.6,7.1,44.6,32.4c27.8,39,38.4,66.3,70.5,61.9c37.2-5.1,43.9,11.9,44.3,34.3c0.2,8.9,0.9,15.3,0.5,20c-0.8,9.8,2.2,19.4,7.5,27.7c13.8,21.4,56.2,23.2,52-20.4c-0.4-3.8-4.5-17-7.7-29.7c-1.8-7.2-5.4-21.2,5.7-29.3c6-4.4,8.5-2.6,15.3-13.6c7.1-11.4,6.7-22.1-7.3-22.4c-8.9-0.2-13.1-0.2-14.9-0.1c-0.7,0-1.3,0.1-2,0.2c-2.5,0.5-13.5-3.7-11.4-29.2c1.5-17.9,12-41.7,18-62.3s46-67,51-69.7")
+            .attr("id","pathLine")
+            .attr("class", "pathLine")
+         
+         //setup line
+         var path = document.getElementById("pathLine");
+         pathLenth = path.getTotalLength();
+         path.style.strokeDasharray  = pathLenth;
+   
+         //make the badges  
+         for(var i = 0; i < pics.length; ++i)
+            makeBadges(draw, pics[i], i);
+         
+         //set progress to 0
+         fillLine(100 );
+         //fillLine(Math.floor(100 * Math.random()));
+         
+         
+      }
+      
+      function fillLine(percentComplete){
+         percentComplete /= 100;
+         var steps  = 100;
+         var length = 6000 * percentComplete;
+         var timeChunk = length/steps
+         
+         for(var i = 1; i <= steps ; ++i){
+            window.setTimeout(changeIt,timeChunk * i, i * percentComplete);
+            
+         }
+         document.getElementById("scale").value = i * percentComplete;
+         	
+      }
+         
+      
+      function changeIt(numIn){
+         
+         var path = document.getElementById("pathLine")
+         var percentComplete =  numIn / 100;
+         
+         //update the line
+         path.style.strokeDashoffset = pathLenth * (1 - percentComplete);
+         
+         //update the pictures
+         var picsToShow = Math.floor(pics.length * percentComplete);
+         for(var i = 0; i < pics.length; ++i){
+            var pic = SVG.get('pic' + i);
+            if(i < picsToShow && !pic.visible()){   
+               pic.front().scale(3).move(552, 150).opacity(0);
+               pic.show();
+               pic.fire('jam:goBack');
+            }
+            else if(i >= picsToShow)
+               pic.hide();
+         }
+      }
+      
+      function makeBadges(draw, picIn, counter){
+         var image = draw.image(picLocation + picIn.fileName).loaded(function(loader) {
+            this.size(loader.width, loader.height);
+            this.center(0,0);
+            this.scale(picIn.scalePic);
+         });
+         
+         
+         //move the group and then add the image
+         var groupMe = draw.group().center(picIn.x, picIn.y).add(image);
+         groupMe.attr("id", 'pic' + counter);
+         groupMe.hide();
+         
+         groupMe.on('jam:goBack', function(){
+               //debugger;
+               this.animate(700).scale(1).move(picIn.x,picIn.y).opacity(1);
+         });
+         
+         groupMe.mouseout(function(){
+               console.count("hi");
+               this.fire("jam:goBack");
+               
+            }
+         );
+         
+         groupMe.mouseover(function(){
+               if(this.transform("scaleX") == 1)
+                  this.front().animate(500).scale(picIn.animateScale).dmove(picIn.dx, picIn.dy); // make it dmove for relative
+            }
+         );
+
+      }
+      
+      function puter(){
+         //get
+         var xIn = document.getElementById("xIn").value;
+         var yIn = document.getElementById("yIn").value;
+         var pic = SVG.get("thisDisk");
+         
+         //put
+         document.getElementById("xOut").innerHTML = xIn;
+         document.getElementById("yOut").innerHTML = yIn;
+         pic.move(xIn, yIn);
+      }
+      
